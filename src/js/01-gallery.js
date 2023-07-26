2ceb4910345b0e678603dfa982fbc6318083cd2f
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-26 22:37:21 +0200

    done first

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryUl = document.querySelector('.gallery');
+createGallery();
+
+function createGallery() {
+  const galleryList = galleryItems.reduce((acc, image) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`
+    );
+  }, '');
+  galleryUl.innerHTML = galleryList;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
