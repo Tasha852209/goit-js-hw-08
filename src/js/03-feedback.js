3a9cd49caa2d199944dedd563293ce25fc8cda40
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-31 11:50:03 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,31 @@
-const message = document.querySelector('textarea');
-const email = document.querySelector('input');
+import throttle from 'lodash.throttle';
 
-console.log(message.value);
-console.log(message.value);
+const form = document.querySelector('.feedback-form');
+
+let emailValue = form.elements.email.value;
+let messageValue = form.elements.message.value;
+
+const data = {
+  email: emailValue,
+  message: messageValue,
+};
+
+function onInput() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+if (localStorage.getItem('feedback-form-state')) {
+  const getData = localStorage.getItem('feedback-form-state');
+  const parsedData = JSON.parse(getData);
+  emailValue = parsedData.email;
+  messageValue = parsedData.message;
+} else {
+  form.addEventListener('input', throttle(onInput, 500));
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit() {
+  localStorage.removeItem('feedback-form-state');
+  reset(form);
+  console.log(data);
+}
