3029cddfa6cdb0655b0ecabc64cc4d600ed16e49
Tasha852209 <112940506+Tasha852209@users.noreply.github.com>
2023-07-31 09:50:27 +0000

    Deploying to gh-pages from @ Tasha852209/goit-js-hw-08@3a9cd49caa2d199944dedd563293ce25fc8cda40 🚀

--- 03-feedback.b37cc4d6.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("textarea");document.querySelector("input");console.log(e.value),console.log(e.value)}();
-//# sourceMappingURL=03-feedback.b37cc4d6.js.map
--- 03-feedback.dba7bcd6.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("textarea");document.querySelector("input");console.log(e.value),console.log(e.value);
-//# sourceMappingURL=03-feedback.dba7bcd6.js.map
