0d9adf6309625ce4d71e7ccdf7de217b8395ee1f
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-07-31 16:44:45 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,28 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 
-const data = {
-  email: '',
-  message: '',
-};
-
 const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
-window.addEventListener('load', onLoad);
-function onLoad() {
-  if (savedData) {
-    data.email = savedData.email;
-    data.message = savedData.message;
-  }
-  return;
+if (savedData) {
+  form.elements.email.value = savedData.email;
+  form.elements.message.value = savedData.message;
 }
 
 form.addEventListener('input', throttle(onInput, 500));
 
-function onInput(event) {
-  data.email = form.elements.email.value;
-  data.message = event.target.value;
-  console.log(event.target.value);
+function onInput() {
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
   console.log(data);
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
 }
-console.log(data);
-console.log(localStorage.getItem('feedback-form-state'));
 form.addEventListener('submit', onSubmit);
 
-function onSubmit() {
+function onSubmit(event) {
+  event.preventDefault();
+  const object = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(object);
   localStorage.removeItem('feedback-form-state');
-  reset(form);
-  console.log(data);
+  event.target.reset();
 }
